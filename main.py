19b286556994acb9e21c5e896625a7c991a70068
BogunAnton <bogunanton687@gmail.com>
2025-03-03 09:49:54 +0700

    7_Задание

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
 
 app = FastAPI()
 
-class UserCreate(BaseModel):
-    name: str = Field(..., min_length=1)
-    email: EmailStr
-    age: int = Field(None, ge=0)
-    is_subscribed: bool = False
+sample_products = [
+    {"product_id": 123, "name": "Smartphone", "category": "Electronics", "price": 599.99},
+    {"product_id": 456, "name": "Phone Case", "category": "Accessories", "price": 19.99},
+    {"product_id": 789, "name": "Iphone", "category": "Electronics", "price": 1299.99},
+    {"product_id": 101, "name": "Headphones", "category": "Accessories", "price": 99.99},
+    {"product_id": 202, "name": "Smartwatch", "category": "Electronics", "price": 299.99}
+]
 
-@app.post("/create_user")
-async def create_user(user: UserCreate):
-    return user
+@app.get("/product/{product_id}")
+async def get_product(product_id: int):
+    for product in sample_products:
+        if product["product_id"] == product_id:
+            return product
+    raise HTTPException(status_code=404, detail="Product not found")
+
+@app.get("/products/search")
+async def search_products(keyword: str, category: Optional[str] = None, limit: Optional[int] = 10):
+    results = [
+        product for product in sample_products
+        if keyword.lower() in product["name"].lower() and
+        (category is None or product["category"].lower() == category.lower())
+    ]
+
+    return results[:limit]
 
 if __name__ == "__main__":
     import uvicorn
